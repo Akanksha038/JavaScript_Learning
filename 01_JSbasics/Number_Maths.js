fce240913870e210112d2c20044a23b79202ae91
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-05-30 12:32:29 +0000

    random Number**

--- 01_JSbasics/Number_Maths.js
+++ 01_JSbasics/Number_Maths.js
@@ -24,4 +24,36 @@ console.log(hundreds.toLocaleString());//OUTPUT IN US STANDARAD ==>100,000,000
 const hundredsIn=100000000;
 console.log(hundredsIn.toLocaleString('en-IN'));//OUTPUT IN INDIAN STANDARAD ==>10,00,00,000
 
+ //*******About maths  =>ye js library ke sath bydifault aati hai */
+//  example=>Pi,  
+
+console.log(Math.abs(-4));//4
+console.log(Math.round(7.9));//8
+
+//ceil==>ceiling matlab top ==>to top wali value choose karega
+console.log(Math.ceil(4.4)); //5
+
+// //floor ye lowest value lega
+console.log(Math.floor(4.9));//4
+
+console.log(Math.min(4,3,6,8)); //3
+
+///////////************8888888 */
+
+console.log(Math.random()); ///ye random values dega 0 and 1 ke beech me
+
+// suppose hamme random value cahaiye 1 and 10 ke beech me to we use random()*10
+// var x=console.log(Math.random()*10); //but ye hamme 0 bhi to de sakta hai like 0.123 aaisa kuch to 0 ko remove karne ke liye we add +1
+console.log(Math.floor((Math.random()*10)+1)); // value in [1,9]
+
+
+//min max definr karne ka
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)));// value in [1,10]
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min); //value in [10,19]
+
 
