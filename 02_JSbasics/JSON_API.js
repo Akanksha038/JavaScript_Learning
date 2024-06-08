ebe87e965438aed99c623bcdad0355ce14065a47
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-06-08 06:47:41 +0000

    added

---
+++ 02_JSbasics/JSON_API.js
@@ -0,0 +1,27 @@
+// Pehle ke time pe, jab modern APIs aur fetch jaisi techniques available nahi thi, tab backend se data ko retrieve karne ke liye alag-alag methods ka use hota tha. Kuch common techniques jo use hoti thi wo hain:
+
+// AJAX (Asynchronous JavaScript and XML):
+// Server-Side Rendering (SSR):
+// iframes:
+// Form Submissions:
+
+// and ye bahut complex hota tha
+
+// but abb modern time pe ye saari values aati hai JSON me
+
+
+
+
+// JSON (JavaScript Object Notation) ek format hai jo data ko easily store aur exchange karne ke liye use hota hai. JSON ka role
+//  modern web development mein bahut important hai,
+//  aur yeh data ko server aur client ke beech easily bhejne mein madad karta hai.
+
+//JSON is just like object , but this object doesn't have any name.it have only {}
+
+// but json me keys and values dono string hote hai and enki value bhi string g=hoti hai
+
+// {
+//     "name":"akanksha",
+//     "age":"22",
+//     "price":"2521"
+// }
